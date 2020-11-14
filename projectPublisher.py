4d885cc11bc2982507ec33c872b92e17f2fbbeb0
ChristineScoot <skotnicznyk@gmail.com>
2020-11-14 12:47:30 +0100

    added traffic light detection

--- projectPublisher.py
+++ projectPublisher.py
@@ -1,16 +1,14 @@
-import base64
 import json
 import os
 import random
 import threading
 from datetime import datetime
 
-import numpy as np
 import paho.mqtt.client as mqtt
-from PIL import Image
+
+from near_location import near_location
 
 # MQTT Settings
-from near_location import near_location
 
 MQTT_Broker = "test.mosquitto.org"
 MQTT_Port = 1883
@@ -44,15 +42,8 @@ mqttc.connect(MQTT_Broker, int(MQTT_Port), int(Keep_Alive_Interval))
 
 def publish_to_topic(topic, message):
     mqttc.publish(topic, message)
-    # print("Published: " + str(message) + " " + "on MQTT Topic: " + str(topic))
-    print("Published: " + "on MQTT Topic: " + str(topic))
+    print("Published: " + message + " on MQTT Topic: " + str(topic))
     print("")
-
-
-def load_image_into_numpy_array(image):
-    (im_width, im_height) = image.size
-    return np.array(image.getdata()).reshape(
-        (im_height, im_width, 3)).astype(np.uint8)
 
 
 def publish_random_sensor_values_to_mqtt():
@@ -60,41 +51,16 @@ def publish_random_sensor_values_to_mqtt():
     image_number = random.randrange(1, num_images)
 
     images_directory = "./frames"
-
     image_path = os.path.join(images_directory, 'img_{}.jpg'.format(image_number))
     print(image_path)
 
-    image = Image.open(image_path)
-    (im_width, im_height) = image.size
-    image_np = np.array(image.getdata()).reshape(
-        (im_height, im_width, 3)).astype(np.uint8)
-    # image_np = np.array(image.getdata()).reshape(
-    #     (im_height, im_width, 3)).tolist()
-    image_np_expanded = np.expand_dims(image_np, axis=0).tolist()
-
-
-
-    # image = Image.open(image_path)
-    # image_np = np.array(image).tolist()
-
-    # FIXME bytes
-    # with open(image_path, mode='rb') as file:
-    #     img = file.read()
-    # image_np = base64.encodebytes(img).decode("utf-8")
-
     threading.Timer(3.0, publish_random_sensor_values_to_mqtt).start()
 
-    humidity_data = {}
-    # humidity_data['Sensor_ID'] = sys.argv[1]
-    humidity_data['Sensor_ID'] = "1"
-    humidity_data['Date'] = (datetime.today()).strftime("%d-%b-%Y %H:%M:%S:%f")
-    humidity_data['Image'] = image_np_expanded
-    humidity_data['Location'] = near_location(50.0646501, 19.9449799, 50)
-    humidity_json_data = json.dumps(humidity_data)
+    date = (datetime.today()).strftime("%d-%b-%Y %H:%M:%S:%f")
+    location = near_location(50.0646501, 19.9449799, 50)
 
-    print(humidity_json_data)
-
-    publish_to_topic(MQTT_Topic_Traffic, humidity_json_data)
+    data = {"Image": image_path, "Sensor_ID": "1", "Date": date, "Location": location}
+    publish_to_topic(MQTT_Topic_Traffic, json.JSONEncoder().encode(data))
 
 
 publish_random_sensor_values_to_mqtt()
--- projectSensorData.py
+++ projectSensorData.py
@@ -1,4 +1,6 @@
 import json
+
+import cv2
 import numpy as np
 import pymongo
 
@@ -6,11 +8,11 @@ import pymongo
 class DatabaseManager():
     def __init__(self):
         client = pymongo.MongoClient(
-            "mongodb+srv://user1:user1@cluster0.f93zw.mongodb.net/cloud?retryWrites=true&w=majority")
+            "mongodb://user1:user1@cluster0-shard-00-00.f93zw.mongodb.net:27017,cluster0-shard-00-01.f93zw.mongodb.net:27017,cluster0-shard-00-02.f93zw.mongodb.net:27017/cloud?ssl=true&replicaSet=atlas-z3y6rw-shard-0&authSource=admin&retryWrites=true&w=majority")
         self.db = client.test
 
     def add_del_update_db_record(self, args=[], name=""):
-        col = self.db.data
+        col = self.db.traffic
         col.insert_many([{'value': i, 'name': name} for i in args])
         return
 
@@ -20,9 +22,100 @@ def load_image_into_numpy_array(image):
     return np.array(image.getdata()).reshape(
         (im_height, im_width, 3)).astype(np.uint8)
 
-# Functions to push Sensor Data into Database
 
 allImages = []
+
+
+def detect(file):
+    img = cv2.imread(file)
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    light_color = None
+    # color range
+    lower_red1 = np.array([0, 100, 100])
+    upper_red1 = np.array([10, 255, 255])
+    lower_red2 = np.array([160, 100, 100])
+    upper_red2 = np.array([180, 255, 255])
+    lower_green = np.array([40, 50, 50])
+    upper_green = np.array([90, 255, 255])
+    lower_yellow = np.array([15, 150, 150])
+    upper_yellow = np.array([35, 255, 255])
+    mask1 = cv2.inRange(hsv, lower_red1, upper_red1)
+    mask2 = cv2.inRange(hsv, lower_red2, upper_red2)
+    maskg = cv2.inRange(hsv, lower_green, upper_green)
+    masky = cv2.inRange(hsv, lower_yellow, upper_yellow)
+    maskr = cv2.add(mask1, mask2)
+
+    size = img.shape
+    # print size
+
+    # hough circle detect
+    r_circles = cv2.HoughCircles(maskr, cv2.HOUGH_GRADIENT, 1, 80,
+                                 param1=50, param2=10, minRadius=0, maxRadius=30)
+
+    g_circles = cv2.HoughCircles(maskg, cv2.HOUGH_GRADIENT, 1, 60,
+                                 param1=50, param2=10, minRadius=0, maxRadius=30)
+
+    y_circles = cv2.HoughCircles(masky, cv2.HOUGH_GRADIENT, 1, 30,
+                                 param1=50, param2=5, minRadius=0, maxRadius=30)
+
+    # traffic light detect
+    r = 5
+    bound = 4.0 / 10
+    if r_circles is not None:
+        r_circles = np.uint16(np.around(r_circles))
+
+        for i in r_circles[0, :]:
+            if i[0] > size[1] or i[1] > size[0] or i[1] > size[0] * bound:
+                continue
+
+            h, s = 0.0, 0.0
+            for m in range(-r, r):
+                for n in range(-r, r):
+
+                    if (i[1] + m) >= size[0] or (i[0] + n) >= size[1]:
+                        continue
+                    h += maskr[i[1] + m, i[0] + n]
+                    s += 1
+            if h / s > 50:
+                light_color = 'RED'
+
+    if g_circles is not None:
+        g_circles = np.uint16(np.around(g_circles))
+
+        for i in g_circles[0, :]:
+            if i[0] > size[1] or i[1] > size[0] or i[1] > size[0] * bound:
+                continue
+
+            h, s = 0.0, 0.0
+            for m in range(-r, r):
+                for n in range(-r, r):
+
+                    if (i[1] + m) >= size[0] or (i[0] + n) >= size[1]:
+                        continue
+                    h += maskg[i[1] + m, i[0] + n]
+                    s += 1
+            if h / s > 100:
+                light_color = 'GREEN'
+
+    if y_circles is not None:
+        y_circles = np.uint16(np.around(y_circles))
+
+        for i in y_circles[0, :]:
+            if i[0] > size[1] or i[1] > size[0] or i[1] > size[0] * bound:
+                continue
+
+            h, s = 0.0, 0.0
+            for m in range(-r, r):
+                for n in range(-r, r):
+
+                    if (i[1] + m) >= size[0] or (i[0] + n) >= size[1]:
+                        continue
+                    h += masky[i[1] + m, i[0] + n]
+                    s += 1
+            if h / s > 50:
+                light_color = 'YELLOW'
+
+    return light_color
 
 
 def DHT22_Image_Handler(json_data):
@@ -31,28 +124,26 @@ def DHT22_Image_Handler(json_data):
     json_dict = json.loads(json_data)
     sensor_id = json_dict['Sensor_ID']
     data_and_time = json_dict['Date']
-    image = json_dict['Image'] #TODO
+    image_path = json_dict['Image']
     location = json_dict['Location']
 
-    # print(image)
-    image_np = load_image_into_numpy_array(image)#TODO
-    # print(image_np)
+    light_color = detect(image_path)
+    print(light_color)
 
-    dbObj = DatabaseManager()
-    allImages.append([sensor_id, data_and_time, image, location]) #TODO
-    # allImages.append([sensor_id, data_and_time, location])
-    if len(allImages) == 10:
-        # FIXME uncomment database
-        # dbObj.add_del_update_db_record(allImages, "Temperature")
-        allImages.clear()
-        del dbObj
-        print("Inserted Image Data into Database.")
-        print("")
+    if light_color is not None:
+        dbObj = DatabaseManager()
+        allImages.append(
+            {"sensor_id": sensor_id, "date": data_and_time, "location": location, "light_color": light_color})
+        if len(allImages) == 10:
+            dbObj.add_del_update_db_record(allImages)
+            allImages.clear()
+            del dbObj
+            print("Inserted Image Data into Database.")
+            print("")
 
 
 # Master Function to Select DB Funtion based on MQTT Topic
 
 def sensor_Data_Handler(Topic, jsonData):
-    print("ok")
     if Topic == "cloud2020/gr04-3/Traffic":
         DHT22_Image_Handler(jsonData)
